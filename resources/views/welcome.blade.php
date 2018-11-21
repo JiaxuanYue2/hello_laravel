af7f748fa008a19157b04637b107e633aa1a8e50
jiaxuan2 <jiaxuan2@ualberta.ca>
2018-11-21 14:45:21 -0700

    Say something

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -35,7 +35,7 @@
     <body>
         <div class="container">
             <div class="content">
-                <div class="title">NIUBI</div>
+                <div class="title">NIUBI_LUNARYUE</div>
             </div>
         </div>
     </body>
